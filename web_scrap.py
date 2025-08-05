30aa26ccb3f6234f1efee340faf8e99210b6b689
Shafah0717 <shafahmuhammed@gmail.com>
2025-08-06 00:01:39 +0530

    basic web scraper with href

--- web_scrap.py
+++ web_scrap.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_website_title(url):
+def get_all_links(url):
     print(f"Getting website: {url}")
     response = requests.get(url)
     if response.status_code==200:
@@ -10,11 +10,22 @@ def get_website_title(url):
         print("website didnt respond quickly")
         return None
     soup = BeautifulSoup(response.content,'html.parser')
-    title=soup.find('title')
-    if title:
-        return title.text.strip()
-    else:
-        return "no title found"
+    link_tags = soup.find_all('a')
+    links=[]
+    for link_tag in link_tags:
+        href = link_tag.get('href')
+
+        text = link_tag.text.strip()
+        href_text = href.replace('//', '')
+        if href and text:
+            links.append({
+                'text':text,
+                'url':href_text
+            })
+    return links
 website_url = "https://www.wikipedia.org"
-title = get_website_title(website_url)
-print(f"The title is: {title}")
+all_links = get_all_links(website_url)
+
+print("First 5 links found:")
+for i, link in enumerate(all_links[:5]):
+    print(f"{i+1}. {link['text']} -> {link['url']}")
